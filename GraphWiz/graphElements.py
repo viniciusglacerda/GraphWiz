a26cff5d86e51b13b9e4eb7897f36c869b510dab
viniciusglacerda <viniciusglacerda2@gmail.com>
2023-09-15 20:56:43 -0300

    refactor: graph elements organization

--- GraphWiz/graphElements.py
+++ GraphWiz/graphElements.py
@@ -1,17 +1,18 @@
-class vertex:
+class ElementBase:
     def __init__(self) -> None:
         self.__label:str
         self.__weight:int
-        self.__neighbors:list[int|str]
     def get_label(self) -> str: return self.__label
     def get_weight(self) -> int: return self.__weight
+    
+class vertex(ElementBase):
+    def __init__(self) -> None:
+        super().__init__()
+        self.__neighbors:list[int|str]
     def get_neighboors(self) -> list[int|str]: return self.__neighbors
 
-class edge:
+class edge(ElementBase):
     def __init__(self) -> None:
-        self.__label:str
-        self.__weight:int
+        super().__init__()
         self.__vertex_pair:list[int|str]
-    def get_label(self) -> str: return self.__label
-    def get_weight(self) -> int: return self.__weight
     def get_vertex_pair(self) -> list[int|str]: return self.__vertex_pair
