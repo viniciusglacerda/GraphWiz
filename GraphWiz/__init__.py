dd68e6bf57bd99ae667a0a2b9b178bfab8d38488
viniciusglacerda <viniciusglacerda2@gmail.com>
2023-09-16 00:33:50 -0300

    test: mod __init__ test

--- GraphWiz/__init__.py
+++ GraphWiz/__init__.py
@@ -1,10 +1,14 @@
 from graph import *
 
 graph = BaseManipulation(4, is_directed=True, vertex_labels=["A", "B", "C", "D"])
-graph.create_edge(1,1)
+graph.create_edge(1,2)
+graph.create_edge(2,3)
+graph.create_edge(3,4)
 graph.create_edge(3,2)
-graph.create_edge(3,3)
-graph.create_edge(4,4)
+graph.create_edge(3,1)
+graph.create_edge(4,1)
+graph.vertex_labeling(1, "T")
 print(graph.count_edge())
 graph.show_matrix()
 graph.show_list()
+graph.edge_weighting(1, 2, 2)
