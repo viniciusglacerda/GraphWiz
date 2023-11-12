760ae0e1013786a1b10dffff7bd2ca93f6bc552d
vinicius.lacerda <vinicius.lacerda@most.com.br>
2023-11-11 22:26:28 -0300

    new tests

--- GraphWiz/__init__.py
+++ GraphWiz/__init__.py
@@ -10,12 +10,14 @@ graph.create_edge(3,4)
 graph.create_edge(4,5)
 
 graph.add_new_vertices(qtd=2, labels=["A", "B"])
-graph.remove_vertice("A")
+# graph.remove_vertice("A")
 
 graph.show_matrix()
 graph.show_list()
 
-print(graph.is_complete())
-graph.vertex_labeling(1, "T")
-print(graph.count_edge())
-graph.edge_weighting("T", 2, 3)
+# print(graph.is_complete())
+# graph.vertex_labeling(1, "T")
+# print(graph.count_edge())
+# graph.edge_weighting("T", 2, 3)
+
+print(graph.bfs())
