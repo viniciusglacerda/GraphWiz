bbdf9ca5631bf189f632aaa879bbe638831cf5a5
vinicius.lacerda <vinicius.lacerda@most.com.br>
2023-11-11 22:25:58 -0300

    fix: bugs related to graph properties and integrating search methods

--- GraphWiz/graph.py
+++ GraphWiz/graph.py
@@ -1,25 +1,38 @@
 from graphElements import Vertex, Edge
+from utils import BFS, DFS
 
 class Graph:
     def __init__(self, qtd_vertices:int=0, is_directed:bool=False, vertices_labels:list[str]=[]) -> None:
         # Deve ser implementado a criação de um grafo com X vértices (o número de vértices deve ser inserido pelo usuário)
         self.qtd_vertices = qtd_vertices
         self.is_directed = is_directed
-        self.vertices:dict[int, Vertex] = {}
+        self.vertex_dict:dict[int, Vertex] = {}
         for index in range(qtd_vertices):
             v = Vertex()
             try: v.label = vertices_labels[index]
             except IndexError: v.label = index+1
-            self.vertices.update({index: v})
+            self.vertex_dict.update({index: v})
             
-        self.edges:list[Edge] = []
+        self.edges_list:list[Edge] = []
     
     @property
     def vertices(self):
-        return self.vertices
+        return self.vertex_dict
+    
+    @vertices.setter
+    def vertices(self, vertices):
+        self.vertex_dict = vertices
+    
+    @property
+    def edges(self):
+        return self.edges_list
+    
+    @edges.setter
+    def edges(self, edges):
+        self.edges_list = edges
 
     def corresponding_edge(self, src_label:str|int, target_label:str|int) -> Edge | None:
-        for edge in self.edges:
+        for edge in self.edges_list:
             src_vertex:Vertex = edge.source
             target_vertex:Vertex = edge.target
             if  src_vertex.label == src_label and target_vertex.label == target_label:
@@ -27,19 +40,19 @@ class Graph:
         return None
     
     def vertex_by_label(self, label:str|int) -> Vertex | None:
-        for vertex in self.vertices.values():
+        for vertex in self.vertex_dict.values():
             if vertex.label == label:
                 return vertex
         return None
             
     def show_matrix(self) -> None:
         print("    ", end="")
-        [print(f"{self.vertices.get(key).label}", end="  ") for key in self.vertices]
+        [print(f"{self.vertex_dict.get(key).label}", end="  ") for key in self.vertex_dict]
         print("\n")
-        for key in self.vertices:
-            print(f"{self.vertices.get(key).label}  ", end="|")
-            for key_j in self.vertices:
-                edge = self.corresponding_edge(self.vertices.get(key).label, self.vertices.get(key_j).label)
+        for key in self.vertex_dict:
+            print(f"{self.vertex_dict.get(key).label}  ", end="|")
+            for key_j in self.vertex_dict:
+                edge = self.corresponding_edge(self.vertex_dict.get(key).label, self.vertex_dict.get(key_j).label)
                 if edge:
                     print(f"{edge.weight}", end="|")
                 else:
@@ -47,29 +60,29 @@ class Graph:
             print("")
 
     def show_list(self) -> None:
-        for vertex in self.vertices.values():
+        for vertex in self.vertex_dict.values():
             print(f"[{vertex.label}] ->", end=" ")
-            for edge in self.edges:
+            for edge in self.edges_list:
                 v1, v2 = edge.source, edge.target
                 if v1.label == vertex.label:
                     print(f"[{v2.label}] ->", end=" ")
             print("")
     
     def add_new_vertices(self, qtd:int=1, labels:list[str|int]=[]) -> None:
-        count = len(self.vertices)
-        for i in range(len(self.vertices), len(self.vertices)+qtd):
+        count = len(self.vertex_dict)
+        for i in range(len(self.vertex_dict), len(self.vertex_dict)+qtd):
             v = Vertex()
             try: v.label = labels[i - count]
             except IndexError: v.label = i+1
-            self.vertices[i] = v
+            self.vertex_dict[i] = v
 
     def remove_vertice(self, vertex_label:str|int) -> None:
         to_remove = []
-        for key in self.vertices:
-            if self.vertices.get(key).label == vertex_label:
+        for key in self.vertex_dict:
+            if self.vertex_dict.get(key).label == vertex_label:
                 to_remove.append(key)
                 
-        self.vertices = {key: value for key, value in self.vertices.items() if key not in to_remove and not None}
+        self.vertex_dict = {key: value for key, value in self.vertex_dict.items() if key not in to_remove and not None}
 
     def create_edge(self, src_label:str|int, target_label:str|int) -> None:
         # Deve ser implementado criação de arestas
@@ -84,15 +97,15 @@ class Graph:
             edge2 = Edge()
             edge2.source = edge.target
             edge2.target = edge.source
-            self.edges.append(edge2)
+            self.edges_list.append(edge2)
 
-        self.edges.append(edge)
+        self.edges_list.append(edge)
 
     def remove_edge(self, src_label:str|int, target_label:str|int) -> None:
         # Remoção de arestas
-        self.edges.remove(self.corresponding_edge(src_label, target_label))
+        self.edges_list.remove(self.corresponding_edge(src_label, target_label))
         if not self.is_directed:
-            self.edges.remove(self.corresponding_edge(target_label, src_label))
+            self.edges_list.remove(self.corresponding_edge(target_label, src_label))
     
     def vertex_weighting(self, vertex_label:str|int, weight:int) -> None:
         # Ponderação de vértices
@@ -140,18 +153,38 @@ class Graph:
 
     def count_edge(self) -> int:
         # Checa a quantidade de arestas
-        length = len(self.edges)
+        length = len(self.edges_list)
         return int(length / 2) if not self.is_directed and length > 0 else length
 
     def count_vertex(self) -> int:
         # Checa a quantidade de vértices
-        return len(self.vertices)
+        return len(self.vertex_dict)
 
     def is_empty(self) -> bool:
         # Checa se o grafo é vazio: um grafo vazio é um grafo com vertices mas sem arestas
-        return False if self.vertices and self.edges else True
+        return False if self.vertex_dict and self.edges_list else True
 
     def is_complete(self) -> bool:
         # Checa se o grafo é completo
-        edge_quantity = len(self.vertices) * (len(self.vertices)-1) / 2
+        edge_quantity = len(self.vertex_dict) * (len(self.vertex_dict)-1) / 2
         return True if self.count_edge() == edge_quantity else False
+    
+    def bfs(self, start_vertex:Vertex=None) -> tuple[dict]:
+        if start_vertex is None:
+            start_vertex = next(iter(self.vertices.values()), None)
+
+        if start_vertex is not None:
+            bfs_instance = BFS(self)
+            return bfs_instance.execute(start_vertex)
+        else:
+            return {}, {}
+    
+    def dfs(self, start_vertex:Vertex=None) -> tuple[dict]:
+        if start_vertex is None:
+            start_vertex = next(iter(self.vertices.values()), None)
+
+        if start_vertex is not None:
+            dfs_instance = DFS(self)
+            return dfs_instance.execute(start_vertex)
+        else:
+            return {}, {}, {}
